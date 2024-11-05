5fde9166ebcd02b2bd78c5c9935c3dab7b3e390c
Alozi <alozimozi@gmail.com>
2024-11-05 18:37:06 +0200

    Start create header for app

---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    console.log("hello");
+    console.log(isEditing);
+    setIsEditing((editing) => !editing);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" value={name} required />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
